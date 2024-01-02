c01dcb3677dce5c1bddaa6d217d76fc90455c684
Nikhil Mowade <91021757+Nik0009@users.noreply.github.com>
2024-01-02 15:38:27 +0530

    Add files via upload

---
+++ adv java/20.11.2023/WebApplication1/src/java/demoServlet.java
@@ -0,0 +1,38 @@
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+
+
+public class demoServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig sc) throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest sr, ServletResponse sr1) throws ServletException, IOException {
+        System.out.println("service");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destory");
+    }
+
+
+}
